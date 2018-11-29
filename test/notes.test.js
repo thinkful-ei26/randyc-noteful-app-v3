33278f86954e4067a2e79b6cc243db216e4334c4
R-Cole <randy@goodglue.com>
2018-11-28 23:22:10 -0700

    test 6

--- test/notes.test.js
+++ test/notes.test.js
@@ -17,38 +17,39 @@ chai.use(chaiHttp);
 
 //MOCHA hooks ////
 
-describe('Notes API', function() {
 
-  before(function () {
 
-    console.log('hello');
+before(function () {
 
-    return mongoose.connect(TEST_MONGODB_URI)
-      .then(() => mongoose.connection.db.dropDatabase());
+  console.log('start hooks');
 
-  });
+  return mongoose.connect(TEST_MONGODB_URI)
+    .then(() => mongoose.connection.db.dropDatabase());
+
+});
      
 
-  beforeEach(function () {
+beforeEach(function () {
 
-    return Note.insertMany(notes);
+  return Note.insertMany(notes);
  
-  });
+});
  
 
-  afterEach(function () {
+afterEach(function () {
 
-    return mongoose.connection.db.dropDatabase();
+  return mongoose.connection.db.dropDatabase();
 
-  });
+});
 
-  after(function () {
+after(function () {
 
-    return mongoose.disconnect();
+  return mongoose.disconnect();
 
-  });
+});
 
-  //TEST 1 passes
+//TEST 1 GET notes count -- passes
+describe('Notes API', function() { 
   it('should return correct number of all the notes', function () {
     
     return chai.request(app)
@@ -60,14 +61,95 @@ describe('Notes API', function() {
         expect(res.body).to.be.a('array');
         expect(res.body).length.to.be.above(0);
 
-        console.log('>>>> ',res.body.length);
+        console.log('number of notes >>>> ',res.body.length);
 
       });
  
 
   });
-
-
 });
+
+//TEST 2 PUT post a new note
+describe('POST /api/notes', function () {
+  it('should create and return a new item when provided valid data', function () {
+    const newItem = {
+      'title': 'The best article about cats ever!',
+      'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...'
+    };
+
+    let res;
+    // 1) First, call the API
+    return chai.request(app)
+      .post('/api/notes')
+      .send(newItem)
+      .then(function (_res) {
+        res = _res;
+        expect(res).to.have.status(201);
+        expect(res).to.have.header('location');
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+        // 2) then call the database
+        return Note.findById(res.body.id);
+      })
+    // 3) then compare the API response to the database results
+      .then(data => {
+        expect(res.body.id).to.equal(data.id);
+        expect(res.body.title).to.equal(data.title);
+        expect(res.body.content).to.equal(data.content);
+        expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
+        expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
+      });
+  });
+});
+
+//TEST 3 GET a note with a certain ID
+describe('GET /api/notes/:id', function () {
+  it('should return correct note', function () {
+    let data;
+    // 1) First, call the database
+    return Note.findOne()
+      .then(_data => {
+        data = _data;
+        // 2) then call the API with the ID
+        return chai.request(app).get(`/api/notes/${data.id}`);
+      })
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+
+        // 3) then compare database results to API response
+        expect(res.body.id).to.equal(data.id);
+        expect(res.body.title).to.equal(data.title);
+        expect(res.body.content).to.equal(data.content);
+        expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
+        expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
+      });
+  });
+});
+
+//TEST 4 GET all notes and compare
+describe('GET /api/notes', function () {
+  // 1) Call the database **and** the API
+  // 2) Wait for both promises to resolve using `Promise.all`
+  return Promise.all([
+    Note.find(),
+    chai.request(app).get('/api/notes')
+  ])
+    // 3) then compare database results to API response
+    .then(([data, res]) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      expect(res.body).to.have.length(data.length);
+    });
+});
+
+
+
+ 
 
 

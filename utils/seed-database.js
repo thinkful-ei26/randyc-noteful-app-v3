e9d77bedf4588d9156d74f73e20b834a310c47a8
R-Cole <randy@goodglue.com>
2018-11-30 10:19:31 -0700

    adjusted seed stuff

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,24 +5,24 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 const Folder = require('../models/folder');
+const Tag = require('../models/tags');
 
-const { folders, notes } = require('../db/seed/data');
+const { folders, notes, tags } = require('../db/seed/data');
  
-
-
-
-
-
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => {
+
     console.info('Dropping Database');
+
     return mongoose.connection.db.dropDatabase();
   })
   .then(() => {
     return Promise.all([
       Note.insertMany(notes),
       Folder.insertMany(folders),
+      Tag.insertMany(tags),
       Folder.createIndexes(),
+      Tag.createIndexes()
     ]);
   })
   .then(() => {
